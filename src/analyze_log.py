85db738481ad7ef137a5c9b723661548c724e591
Andrey Visniewski <cortaesai@gmail.com>
2023-02-10 15:26:00 -0300

    chore: (Requisito 01) - cria funcao auxiliar para escrever o arquivo .txt

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -3,13 +3,21 @@ import sys
 
 def analyze_log(path_to_file):
     print('path_to_file: ', path_to_file)
-    if path_to_file != "data/orders_1.csv":
-        raise FileNotFoundError("Arquivo inexistente: '{path_to_file}'")
 
-    if path_to_file[len(path_to_file) - 4:] != ".csv":
-        return print("Extensão inválida: '{path_to_file}'", file=sys.stderr)
+    try:
+        if path_to_file[len(path_to_file) - 4:] != ".csv":
+            raise FileNotFoundError("Extensão inválida: '{path_to_file}'")
+        csv_list = ""
+        with open(path_to_file, encoding= "utf-8") as file:
+            csv_data = csv.DictReader(file, delimiter=",", quotechar='"')
+            csv_list = list(csv_data)
+            return write_txt(csv_list)
 
-    with open(path_to_file, encoding= "utf-8") as file:
-        csv_data = csv.DictReader(file, delimiter=",", quotechar='"')
-        csv_list = list(csv_data)
-            # print(csv_list)
+    except FileNotFoundError:
+        print("Arquivo inexistente: '{path_to_file}'", file=sys.stderr)
+
+
+def write_txt(param):
+    with open("data/mkt_campaign.txt", "w") as file:
+        for line in param:
+            file.write(str(line) + "\n")
