3b9546c245b65e6862cab62fb752703981d65612
Andrey Visniewski <cortaesai@gmail.com>
2023-02-10 17:52:29 -0300

    chore: (Requisito 01) adiciona funcao que mostra quais pratos joao nunca pediu

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -18,7 +18,8 @@ def read_csv(path_to_file):
     with open(path_to_file, encoding="utf-8") as file:
         csv_data = csv.reader(file, delimiter=",", quotechar='"')
         csv_list = list(tuple(line) for line in csv_data)
-        return write_txt(csv_list)
+        john_never_asked(csv_list)
+        return csv_list
 
 
 def write_txt(param):
@@ -26,3 +27,24 @@ def write_txt(param):
     with open("data/mkt_campaign.txt", "w") as file:
         for line in param:
             file.write(str(line) + "\n")
+
+
+def john_never_asked(list_tuple):
+    dict_snacks = {}
+    john_never_eaten = {}
+    for a, b, c in list_tuple:
+        if b not in dict_snacks:
+            dict_snacks[b] = set()
+        # if b not in dict_snacks:
+        #     dict_snacks[a] = set()
+        dict_snacks[b].add(a)
+        # dict_snacks[a].add(b)
+    
+    print('dict_snacks: ', dict_snacks)
+    
+    # dict_values = dict_snacks.values()
+    # pessoas = [item for item in dict_values]
+    # print('pessoa: ', pessoas)
+ 
+            
+    return john_never_eaten
