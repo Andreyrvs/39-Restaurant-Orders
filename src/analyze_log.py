44cb8cbfdf1524cba64dbeab9bed126415faeb30
Andrey Visniewski <cortaesai@gmail.com>
2023-02-10 15:43:19 -0300

    fix: linter

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,20 +1,26 @@
 import csv
 import sys
 
+
 def analyze_log(path_to_file):
     print('path_to_file: ', path_to_file)
 
     try:
         if path_to_file[len(path_to_file) - 4:] != ".csv":
-            raise FileNotFoundError("Extensão inválida: '{path_to_file}'")
-        csv_list = ""
-        with open(path_to_file, encoding= "utf-8") as file:
-            csv_data = csv.DictReader(file, delimiter=",", quotechar='"')
-            csv_list = list(csv_data)
-            return write_txt(csv_list)
+            # raise FileNotFoundError(f"Extensão inválida: {path_to_file}")
+            return print(f"Extensão inválida: {path_to_file}", file=sys.stderr)
+        read_csv(path_to_file)
 
     except FileNotFoundError:
-        print("Arquivo inexistente: '{path_to_file}'", file=sys.stderr)
+        print(f"Arquivo inexistente: {path_to_file}", file=sys.stderr)
+
+
+def read_csv(path_to_file):
+
+    with open(path_to_file, encoding="utf-8") as file:
+        csv_data = csv.DictReader(file, delimiter=",", quotechar='"')
+        csv_list = list(csv_data)
+        return write_txt(csv_list)
 
 
 def write_txt(param):
