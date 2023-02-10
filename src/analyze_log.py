891114c29ba9bf120069301395ad196e8049ad86
Andrey Visniewski <cortaesai@gmail.com>
2023-02-10 15:58:38 -0300

    chore: (Requisito 01) - Gera os erros corretamente

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -5,14 +5,13 @@ import sys
 def analyze_log(path_to_file):
     print('path_to_file: ', path_to_file)
 
+    if path_to_file[len(path_to_file) - 4:] != ".csv":
+        raise FileNotFoundError(f"Extensão inválida: {path_to_file}")
     try:
-        if path_to_file[len(path_to_file) - 4:] != ".csv":
-            # raise FileNotFoundError(f"Extensão inválida: {path_to_file}")
-            return print(f"Extensão inválida: {path_to_file}", file=sys.stderr)
         read_csv(path_to_file)
 
     except FileNotFoundError:
-        print(f"Arquivo inexistente: {path_to_file}", file=sys.stderr)
+        raise FileNotFoundError(f"Arquivo inexistente: {path_to_file}")
 
 
 def read_csv(path_to_file):
@@ -24,6 +23,7 @@ def read_csv(path_to_file):
 
 
 def write_txt(param):
+
     with open("data/mkt_campaign.txt", "w") as file:
         for line in param:
             file.write(str(line) + "\n")
