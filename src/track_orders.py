940ca831d7e0ef8227b86601e23656d41b2f2a27
Andrey Visniewski <cortaesai@gmail.com>
2023-02-17 09:54:10 -0300

    feat: (Requisito 02) get_most_ordered_dish_per_cusomer - Inicia implementacão

--- src/track_orders.py
+++ src/track_orders.py
@@ -10,7 +10,19 @@ class TrackOrders:
         return new_order
 
     def get_most_ordered_dish_per_customer(self, customer):
-        pass
+        print('customer: ', customer, self._orders)
+        all_orders = set()
+        customer_orders = set()
+
+        for order in self._orders:
+            order = order['order']
+            all_orders.add(order)
+
+            if order['customer'] == customer:
+                customer_orders.add(order)
+
+        pratos_nao_pedidos = all_orders - customer_orders
+        return pratos_nao_pedidos
 
     def get_never_ordered_per_customer(self, customer):
         pass
