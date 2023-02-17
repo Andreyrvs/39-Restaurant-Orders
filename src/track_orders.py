f5fbc39f03c285eda85adf9e4a46923b8a124c98
Andrey Visniewski <cortaesai@gmail.com>
2023-02-17 10:04:03 -0300

    feat: (Requisito 02) get_most_ordered_dish_per_cusomer - Refatora utilizando compreensão de lista para tornar o código mais legível

--- src/track_orders.py
+++ src/track_orders.py
@@ -1,30 +1,25 @@
 class TrackOrders:
     def __init__(self):
-        self._orders = list()
+        self.orders = list()
 
     def __len__(self):
-        return len(self._orders)
+        return len(self.orders)
 
     def add_new_order(self, customer, order, day):
-        new_order = self._orders.append(
+        new_order = self.orders.append(
             {'customer': customer, 'order': order, 'day': day}
         )
         return new_order
 
     def get_most_ordered_dish_per_customer(self, customer):
-        print('customer: ', customer, self._orders)
-        all_orders = set()
-        customer_orders = set()
+        customer_orders = [
+            order['order'] for order in self.orders if order['customer'] == customer
+        ]
 
-        for order in self._orders:
-            order = order['order']
-            all_orders.add(order)
-
-            if order['customer'] == customer:
-                customer_orders.add(order)
-
-        pratos_nao_pedidos = all_orders - customer_orders
-        return pratos_nao_pedidos
+        if not customer_orders:
+            return None
+        
+        return max(set(customer_orders), key=customer_orders.count)
 
     def get_never_ordered_per_customer(self, customer):
         pass
